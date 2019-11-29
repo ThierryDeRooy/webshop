2815a365a027a4c3056a02d1e947f70625e590dd
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-11-29 21:12:02 +0100

    Update Message.java

--- src/main/java/com/webshop/kung/entity/Message.java
+++ src/main/java/com/webshop/kung/entity/Message.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="item_id")
     private Long id;
 
