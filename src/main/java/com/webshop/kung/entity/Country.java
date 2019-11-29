39937d4b84114049667acb60895f23d070445ffe
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-11-29 21:11:44 +0100

    Update Country.java

--- src/main/java/com/webshop/kung/entity/Country.java
+++ src/main/java/com/webshop/kung/entity/Country.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 public class Country {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="country_id")
     private Long id;
 
