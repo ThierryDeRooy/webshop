f681dc2c1e66b2516e684e914c0c822d22ba165e
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-11-29 21:11:06 +0100

    Update Admin.java

--- src/main/java/com/webshop/kung/entity/Admin.java
+++ src/main/java/com/webshop/kung/entity/Admin.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.NotNull;
 public class Admin {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="customer_id")
     private Long id;
 
