e5e0e098c6c556d89b8b61a3f28c8540c4623840
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-11-29 21:12:48 +0100

    Update OrderDetails.java

--- src/main/java/com/webshop/kung/entity/OrderDetails.java
+++ src/main/java/com/webshop/kung/entity/OrderDetails.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public class OrderDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private Long id;
 
