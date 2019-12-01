6f8106aea56ff9ad5ae15fb6dfd2303e0b77ef75
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-12-01 18:43:32 +0100

    Add files via upload

--- src/main/java/com/webshop/kung/repository/ProductRepository.java
+++ src/main/java/com/webshop/kung/repository/ProductRepository.java
@@ -14,19 +14,18 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findByLangAndName(Locale lang, String name);
     List<Product> findByLang(Locale lang);
 
-
     @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.STATUS=:status AND P.LANG=:locale AND PD.CODE=P.CODE", nativeQuery = true)
-    List<Product> findByLangAndStatus(String locale, Integer status);
+    List<Product> getByLangAndStatus(String locale, Integer status);
 
     @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.CODE=:code AND P.LANG=:locale AND PD.CODE=P.CODE", nativeQuery = true)
-    Product findByCode(String locale, String code);
+    Product getByCode(String locale, String code);
 
     @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.CODE=:code AND P.LANG=:locale AND PD.CODE=P.CODE AND PD.STATUS=:status", nativeQuery = true)
-    Product findByCodeAndStatus(String locale, String code, Integer status);
+    Product getByCodeAndStatus(String locale, String code, Integer status);
 
     @Query(value = "SELECT P.* FROM PRODUCT P JOIN LOCALISED_STRINGS L WHERE P.NAME_ID=L.LOCALISED_ID AND L.STRINGS_KEY=:locale AND L.STRINGS LIKE %:name%", nativeQuery = true)
-    List<Product> findByName(String locale, String name);
+    List<Product> getByName(String locale, String name);
 
     @Query(value = "SELECT * FROM PRODUCT prod where prod.LANG<>:locale AND prod.CODE NOT in (SELECT prod1.CODE FROM PRODUCT prod1 where prod1.lang = :locale);", nativeQuery = true)
-    List<Product> findMissingProducts(String locale);
+    List<Product> getMissingProducts(String locale);
 }
