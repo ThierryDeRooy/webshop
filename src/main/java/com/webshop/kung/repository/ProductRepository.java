2f230e32406ca3fdaeffde210ce343e20cc29a9d
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-12-01 19:09:14 +0100

    Add files via upload

--- src/main/java/com/webshop/kung/repository/ProductRepository.java
+++ src/main/java/com/webshop/kung/repository/ProductRepository.java
@@ -14,18 +14,18 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findByLangAndName(Locale lang, String name);
     List<Product> findByLang(Locale lang);
 
-    @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.STATUS=:status AND P.LANG=:locale AND PD.CODE=P.CODE", nativeQuery = true)
+    @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.status=:status AND P.lang=:locale AND PD.code=P.code", nativeQuery = true)
     List<Product> getByLangAndStatus(String locale, Integer status);
 
-    @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.CODE=:code AND P.LANG=:locale AND PD.CODE=P.CODE", nativeQuery = true)
+    @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.code=:code AND P.lang=:locale AND PD.code=P.code", nativeQuery = true)
     Product getByCode(String locale, String code);
 
-    @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.CODE=:code AND P.LANG=:locale AND PD.CODE=P.CODE AND PD.STATUS=:status", nativeQuery = true)
+    @Query(value = "SELECT P.* FROM PRODUCT P JOIN PRODUCT_DETAILS PD WHERE PD.code=:code AND P.lang=:locale AND PD.code=P.code AND PD.status=:status", nativeQuery = true)
     Product getByCodeAndStatus(String locale, String code, Integer status);
 
     @Query(value = "SELECT P.* FROM PRODUCT P JOIN LOCALISED_STRINGS L WHERE P.NAME_ID=L.LOCALISED_ID AND L.STRINGS_KEY=:locale AND L.STRINGS LIKE %:name%", nativeQuery = true)
     List<Product> getByName(String locale, String name);
 
-    @Query(value = "SELECT * FROM PRODUCT prod where prod.LANG<>:locale AND prod.CODE NOT in (SELECT prod1.CODE FROM PRODUCT prod1 where prod1.lang = :locale);", nativeQuery = true)
+    @Query(value = "SELECT * FROM PRODUCT prod where prod.lang<>:locale AND prod.code NOT in (SELECT prod1.code FROM PRODUCT prod1 where prod1.lang = :locale);", nativeQuery = true)
     List<Product> getMissingProducts(String locale);
 }
