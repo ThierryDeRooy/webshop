54e8b936d5ec21040cba19dea0b47efaa28b8b60
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-12-02 20:21:58 +0100

    Update ProductRepository.java

--- src/main/java/com/webshop/kung/repository/ProductRepository.java
+++ src/main/java/com/webshop/kung/repository/ProductRepository.java
@@ -14,18 +14,18 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findByLangAndName(Locale lang, String name);
     List<Product> findByLang(Locale lang);
 
-    @Query("SELECT P FROM product P, product_details PD WHERE PD.status=:status AND P.lang=:locale AND PD.code=P.code")
+    @Query("SELECT P FROM Product P, productDetails PD WHERE PD.status=:status AND P.lang=:locale AND PD.code=P.code")
     List<Product> getByLangAndStatus(String locale, Integer status);
 
-    @Query("SELECT P FROM product P, product_details PD WHERE PD.code=:code AND P.lang=:locale AND PD.code=P.code")
+    @Query("SELECT P FROM Product P, productDetails PD WHERE PD.code=:code AND P.lang=:locale AND PD.code=P.code")
     Product getByCode(String locale, String code);
 
-    @Query("SELECT P FROM product P, product_details PD WHERE PD.code=:code AND P.lang=:locale AND PD.code=P.code AND PD.status=:status")
+    @Query("SELECT P FROM Product P, ProductDetails PD WHERE PD.code=:code AND P.lang=:locale AND PD.code=P.code AND PD.status=:status")
     Product getByCodeAndStatus(String locale, String code, Integer status);
 
 //    @Query("SELECT P.* FROM product P, LOCALISED_STRINGS L WHERE P.NAME_ID=L.LOCALISED_ID AND L.STRINGS_KEY=:locale AND L.STRINGS LIKE %:name%")
 //    List<Product> getByName(String locale, String name);
 
-    @Query("SELECT prod FROM product prod where prod.lang<>:locale AND prod.code NOT in (SELECT prod1.code FROM product prod1 where prod1.lang=:locale)")
+    @Query("SELECT prod FROM Product prod where prod.lang<>:locale AND prod.code NOT in (SELECT prod1.code FROM Product prod1 where prod1.lang=:locale)")
     List<Product> getMissingProducts(String locale);
 }
