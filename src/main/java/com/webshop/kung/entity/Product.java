108760d67696a7b837a47384d6c26907e02b920d
ThierryDeRooy <38113907+ThierryDeRooy@users.noreply.github.com>
2019-12-04 21:57:51 +0100

    Update Product.java

--- src/main/java/com/webshop/kung/entity/Product.java
+++ src/main/java/com/webshop/kung/entity/Product.java
@@ -26,7 +26,7 @@ public class Product implements Serializable {
     private Category category;
 
     @Valid
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "details_id", nullable = false)
     private ProductDetails productDetails;
 
